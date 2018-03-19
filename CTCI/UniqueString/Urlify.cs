6ca725cb4924f48246c2beb0e195472b345580a6
PranavBhat <pranavbhat.1992@gmail.com>
2018-03-19 23:52:54 +0530

    CTCI: Strings&Arrays: URLify

--- CTCI/UniqueString/Urlify.cs
+++ CTCI/UniqueString/Urlify.cs
@@ -10,30 +10,36 @@ namespace UniqueString
     {
         static void Main(string[] args)
         {
-            MyVersion();
-            //CTCIVersion();
+            //MyVersion();
+            CTCIVersion();
         }
 
         public static void MyVersion()
         {
             /*
-                Input: "Mr John Smith      ", 13
+                Input: "Mr John Smith    ", 13
                 Output: "Mr%20John%20Smith"
             */
 
-            char[] charArr = "Mr John Smith      ".ToCharArray();
-            var len = 13;
+            char[] charArr = "Mr John Smith    ".ToCharArray();
+            var len = 12;   // Truelength - 1
 
-            for (int i = 13; i > 0; i--)
+            var i = charArr.Length - 1;
+
+            while (i >= 0)
             {
-                if (charArr[i] != ' ')
+                if (charArr[len] != ' ')
                 {
-                    continue;
+                    charArr[i--] = charArr[len];
                 }
                 else
                 {
-                    charArr[i] = '%';                    
+                    charArr[i--] = '0';
+                    charArr[i--] = '2';
+                    charArr[i--] = '%';
                 }
+
+                len--;
             }
 
             Console.WriteLine(charArr);
@@ -43,6 +49,42 @@ namespace UniqueString
 
         public static void CTCIVersion()
         {
+            char[] str = "Mr John Smith    ".ToCharArray();
+            var trueLength = 13;
+            int spaceCount = 0, index;
+
+            for (var i = 0; i < trueLength; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    spaceCount++;
+
+                }
+            }
+
+            index = trueLength + spaceCount * 2;
+
+            if (trueLength < str.Length) str[trueLength] = '\0';
+
+            for (var i = trueLength-1; i >= 0; i--)
+            {
+                if (str[i] == ' ')
+                {
+                    str[index - 1] = '0';
+                    str[index - 2] = '2';
+                    str[index - 3] = '%';
+                    index -= 3;
+                }
+                else
+                {
+                    str[index - 1] = str[i];
+                    index--;
+                }
+            }
+
+
+            Console.WriteLine(str);
+            Console.ReadLine();
 
         }
     }
